a093dcaef02c4eb158bcb4965aa80ecfdcfaa10f
ROAMTECH <jeremiah.dindi@roamtech.com>
2023-09-17 22:58:30 +0300

    feat-backend controllers and models

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\IndexController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +16,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware('auth')
+    ->group(function () {
+
+        Route::get('/', [IndexController::class, 'index'])->name('home');
+        Route::get('/about', [IndexController::class, 'about'])->name('about');
+
+        Route::resource('users', UserController::class);
 });
+
+Route::controller(AuthController::class)
+    ->group(function () {
+    Route::post('/login', 'login')->name('login')->middleware('guest');
+    Route::delete('/logout', 'logout')->name('logout')->middleware('auth');
+});
+
+
+Route::inertia('/login', 'Login');
